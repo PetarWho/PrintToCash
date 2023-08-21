297708bb755bb2da2cd7603bd828df7978171809
Roy <92612927+PetarWho@users.noreply.github.com>
2023-08-21 05:27:41 +0300

    Initial commit

--- AppData/Configuration/ConnectionString/ConnectionString.cs
+++ AppData/Configuration/ConnectionString/ConnectionString.cs
@@ -2,6 +2,6 @@
 {
     internal class ConnectionString
     {
-        public static string MyConnectionString = @"Server=ROY\SQLEXPRESS;Database=LeagueChampsDb;Trusted_Connection=True";
+        public static string MyConnectionString = @"Server=ROY\SQLEXPRESS;Database=PrintToCash;Trusted_Connection=True";
     }
 }
